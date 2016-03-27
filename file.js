6b76557880637492edefe881b8c0d19f4f6a219d
Ozawa Masayuki <ozawa@coroq.com>
2016-03-27 13:42:07 +0900

    ファイル ローテーション

--- file.js
+++ file.js
@@ -7,27 +7,53 @@ var FileManager = function() {
   });
 };
 FileManager.prototype.open = function(path) {
-  var activateFile = function(file) {
-    $("#files .file-item.active").removeClass("active");
-    file.addClass("active");
-    editor_manager.activate(file.data("path"));
-  };
-  // search for opening file
+  var self = this;
+  // try to activate opening files
+  if (this.activate(path)) {
+    return;
+  }
+  editor_manager.open(path).then(function() {
+    var dir = path.replace(new RegExp("[^/]+$"), "");
+    var name = path.replace(new RegExp(".*/"), "");
+    $("<div>").data("path", path).addClass("file-item").append(
+      $("<div>").addClass("dir").text(dir),
+      $("<div>").addClass("name").text(name)
+    ).appendTo("#files");
+    self.activate(path);
+  });
+};
+FileManager.prototype.activate = function(path) {
   var file = $("#files .file-item").filter(function(idx, item) {
     return $(item).data("path") == path;
   });
-  if (file.length) {
-    activateFile(file);
-  } else {
-    var dir = path.replace(new RegExp("[^/]+$"), "");
-    var name = path.replace(new RegExp(".*/"), "");
-    editor_manager.open(path).then(function() {
-      file = $("<div>").data("path", path).addClass("file-item").append(
-        $("<div>").addClass("dir").text(dir),
-        $("<div>").addClass("name").text(name)
-      ).appendTo("#files");
-      activateFile(file);
-    });
- }
+  if (file.length == 0) {
+    return false;
+  }
+  $("#files .file-item.active").removeClass("active");
+  file.addClass("active");
+  editor_manager.activate(path);
+  return true;
 };
+FileManager.prototype.nextFile = function() {
+  this.rotateFile(true);
+};
+FileManager.prototype.prevFile = function() {
+  this.rotateFile(false);
+};
+FileManager.prototype.rotateFile = function(next) {
+  var dir = next ? "next" : "prev";
+  var target = $("#files .file-item.active")[dir]();
+  if (target.length == 0) {
+    dir = next ? "first" : "last";
+    target = $("#files .file-item")[dir]();
+    if (target.length == 0) {
+      return;
+    }
+  }
+  $("#files .file-item.active").removeClass("active");
+  target.addClass("active");
+  editor_manager.activate(target.data("path"));
+};
+
+
 var file_manager = new FileManager();
