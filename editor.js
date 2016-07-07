58dd7e1f1c49b51de0faf514c2bbe5a41dfb5eac
Ozawa Masayuki <ozawa@coroq.com>
2016-07-07 11:33:55 +0900

    Maintain indentation on paste

--- editor.js
+++ editor.js
@@ -56,6 +56,37 @@ EditorManager.prototype.open = function(path) {
           "Ctrl-D": false,
           "Cmd-D": false,
         });
+        // maintain indentation on paste
+        code_mirror.on("beforeChange", function(cm, change) {
+          if (change.origin != "paste") {
+            return;
+          }
+          if (CodeMirror.cmpPos(change.from, change.to)) {
+            return;
+          }
+          // check if the insertion point is at the end of the line
+          var dest = cm.getLine(change.from.line);
+          if (dest.length != change.from.ch) {
+            return;
+          }
+          // check if the line consists of only white spaces
+          if (dest.match(/[^ \t]/)) {
+            return;
+          }
+          // remove the last empty line
+          if (change.text[change.text.length - 1] == "") {
+            change.text.pop();
+          }
+          var base_indent = change.text[0].match(/^[ \t]*/)[0];
+          change.text = change.text.map(function(line, i) {
+            line = line.match(/^([ \t]*)(.*)/);
+            var indent = line[1];
+            var text = line[2];
+            indent = (dest + base_indent).substr(0, dest.length + indent.length - base_indent.length);
+            return indent + text;
+          });
+          change.text[0] = change.text[0].substr(dest.length);
+        });
         code_mirror.on("changes", function() {
           autoSave();
           file_manager.setStatus(
