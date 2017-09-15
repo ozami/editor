5a78a17e9b665fa840ca604897d780384b3a0b0e
Ozawa Masayuki <ozawa@coroq.com>
2017-09-16 00:24:24 +0900

    Use React for footer of Editor

--- js/editor-view.js
+++ js/editor-view.js
@@ -1,3 +1,5 @@
+const React = require("react")
+const ReactDOM = require("react-dom")
 var $ = require("jquery")
 var CodeMirror = require("./codemirror")
 var SelectEncodingDialogView = require("./select-encoding-dialog-view")
@@ -10,17 +12,34 @@ var EditorView = function($root, editor, editor_mgr) {
     mode: editor.mode.get(),
   })
   
-  // footer
-  $root.append(
-    $('<div class="editor-foot">').append(
-      $('<div class="editor-message">'),
-      $('<button class="editor-indent link" type="button">'),
-      $('<button class="editor-eol link" type="button">'),
-      $('<button class="editor-encoding link" type="button">'),
-      $('<div class="editor-mode">')
+  const $footer = $('<div>').appendTo($root)
+  const render = () => {
+    const eol_names = {
+      "\r": "CR",
+      "\n": "LF",
+      "\r\n": "CRLF",
+    }
+    ReactDOM.render(
+      <div className="editor-foot">
+        <div className="editor-message">{editor.message.get()}</div>
+        <button className="editor-indent link" type="button" onClick={editor.indent.rotate}>
+          {editor.indent.get()}
+        </button>
+        <button className="editor-eol link" type="button" onClick={file.eol.rotate}>
+          {eol_names[file.eol.get()]}
+        </button>
+        <button className="editor-encoding link" type="button"
+           onClick={() => editor.select_encoding_dialog.show(file.encoding.get())}>
+          {file.encoding.get()}
+        </button>
+        <div className="editor-mode">
+          {editor.mode.get()}
+        </div>
+      </div>,
+      $footer[0]
     )
-  )
-  
+  }
+  render()
   SelectEncodingDialogView(
     $('<div>').appendTo($root),
     editor.select_encoding_dialog
@@ -47,17 +66,14 @@ var EditorView = function($root, editor, editor_mgr) {
   })
 
   // mode
-  var updateMode = function(mode) {
+  editor.mode.observe((mode) => {
     cm.setOption("mode", mode)
     CodeMirror.registerHelper("hintWords", mode, null)
-    $root.find(".editor-mode").text(mode)
-  }
-  editor.mode.observe(updateMode)
-  updateMode(editor.mode.get())
+    render()
+  })
   
   // indent
-  var updateIndent = function(type) {
-    $root.find(".editor-indent").text(type)
+  editor.indent.observe((type) => {
     if (type == "TAB") {
       cm.setOption("indentWithTabs", true)
       cm.setOption("indentUnit", 4)
@@ -66,47 +82,15 @@ var EditorView = function($root, editor, editor_mgr) {
       cm.setOption("indentWithTabs", false)
       cm.setOption("indentUnit", Number(type.replace("SP", "")))
     }
-  }
-  editor.indent.observe(updateIndent)
-  updateIndent(editor.indent.get())
-  $root.find(".editor-indent").click(function() {
-    editor.indent.rotate()
+    render()
   })
   
   // line seprator
-  var updateEol = function(eol) {
-    var names = {
-      "\r": "CR",
-      "\n": "LF",
-      "\r\n": "CRLF",
-    }
-    $root.find(".editor-eol").text(names[eol])
-  }
-  file.eol.observe(updateEol)
-  updateEol(file.eol.get())
-  $root.find(".editor-eol").click(function() {
-    file.eol.rotate()
-  })
+  file.eol.observe(render)
   
   // encoding
-  var updateEncoding = function(encoding) {
-    $root.find(".editor-encoding").text(encoding)
-  }
-  file.encoding.add(updateEncoding)
-  updateEncoding(file.encoding.get())
-  $root.find(".editor-encoding").click(function() {
-    editor.select_encoding_dialog.show(
-      file.encoding.get()
-    )
-  })
-  editor.select_encoding_dialog.confirmed.add(function(encoding) {
-    file.encoding.set(encoding)
-  })
-  
-  // message
-  editor.message.observe(function(message) {
-    $root.find(".editor-message").text(message)
-  })
+  file.encoding.add(render)
+  editor.select_encoding_dialog.confirmed.add(file.encoding.set)
   
   // active
   editor_mgr.activated.add(function(active) {
