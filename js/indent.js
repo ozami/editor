cb9620fab43933ab999db24afc0aa9ae102c7369
Ozawa Masayuki <ozawa@coroq.com>
2017-12-28 23:03:14 +0900

    Change default indent size to two

--- js/indent.js
+++ js/indent.js
@@ -1,21 +1,20 @@
 var Rotate = require("./rotate")
 
 var Indent = function(type) {
-  return Rotate(["4SP", "2SP", "TAB"], type)
+  return Rotate(["2SP", "4SP", "TAB"], type)
 }
 
 Indent.detectIndentType = function(content) {
-  if (content.match(/[\r\n]+\t/)) {
+  if (content.match(/\n\t[^\n]/)) {
     return "TAB"
   }
-  var lines = content.split(/[\r\n]+/)
-  for (var i = 0; i < lines.length; ++i) {
-    var indent = lines[i].replace(/^( *).*/, "$1")
-    if (indent.length == 2) {
-      return "2SP"
+  for (var size = 2; size <= 4; size *= 2) {
+    var rx = new RegExp("^ {" + size + "}[^ \n]", "m")
+    if (rx.test(content)) {
+      return size + "SP"
     }
   }
-  return "4SP"
+  return "2SP"
 }
 
 module.exports = Indent
