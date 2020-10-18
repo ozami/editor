8067b0c9aa13a92b777224f2f652d83659a2a3c4
Ozawa Masayuki <ozawa@coroq.com>
2020-10-18 15:02:59 +0900

    Auto open block in hinting

--- js/codemirror/hinter.js
+++ js/codemirror/hinter.js
@@ -1,35 +1,48 @@
 const CodeMirror = require("codemirror")
 
+function makeApplyHintFunction(move_line, move_ch) {
+  return function(code_mirror, self, data) {
+    code_mirror.replaceRange(data.text, self.from, self.to)
+    for (let x = 1; x < data.text.split("\n").length; ++x) {
+      code_mirror.indentLine(self.to.line + x)
+    }
+    code_mirror.setCursor(self.from.line + move_line, self.from.ch + move_ch)
+  }
+}
+
 const keywords = [
-  ["function", "function() {}", "function() {}"],
-  ["foreach", "foreach() {}", "foreach() {}"],
-  ["for", "for () {}", "for () {}"],
-  ["class", "class {}", "class {}"],
-  ["if", "if () {}", "if () {}"],
-  ["else", "else {}", "else {}"],
+  // keyword, display text, insert text, hint function
+  ["function", "function {}", "function() {\n\n}", makeApplyHintFunction(0, 9)],
+  ["foreach", "foreach {}", "foreach () {\n\n}", makeApplyHintFunction(0, 9)],
+  ["for", "for {}", "for () {\n\n}", makeApplyHintFunction(0, 5)],
+  ["class", "class {}", "class  {\n\n}", makeApplyHintFunction(0, 6)],
+  ["if", "if {}", "if () {\n\n}", makeApplyHintFunction(0, 4)],
+  ["else", "else {}", "else {\n\n}", makeApplyHintFunction(0, 6)],
   "break",
   "continue",
   "throw new",
   "var",
   "let",
   "const",
-  ["-php", "<?php ?>", "<?php  ?>"],
-  ["-print", "<?php p() ?>", "<?php p() ?>"],
-  ["-div", "<div>", "<div></div>"],
-  ["-p", "<p>", "<p></p>"],
-  ["-span", "<span>", "<span></span>"],
-  ["-a", "<a>", '<a href=""></a>'],
-  ["-h1", "<h1>", '<h1></h1>'],
-  ["-h2", "<h2>", '<h2></h2>'],
-  ["-h3", "<h3>", '<h3></h3>'],
-  ["-h4", "<h4>", '<h4></h4>'],
-  ["-h5", "<h5>", '<h5></h5>'],
-  ["-ul", "<ul>", '<ul></ul>'],
-  ["-li", "<li>", '<li></li>'],
-  ["-label", "<label>", '<label></label>'],
-  ["-form", "<form>", '<form></form>'],
-  ["-button", "<button>", '<button></button>'],
-  "compact()",
+  ["<div", "<div>", "<div>\n\n</div>", makeApplyHintFunction(1, 0)],
+  ["<p", "<p>", "<p>\n\n</p>", makeApplyHintFunction(1, 0)],
+  ["<span", "<span>", "<span></span>", makeApplyHintFunction(0, 6)],
+  ["<a", "<a>", '<a href=""></a>', makeApplyHintFunction(0, 9)],
+  ["<h1", "<h1>", '<h1>\n\n</h1>', makeApplyHintFunction(1, 0)],
+  ["<h2", "<h2>", '<h2>\n\n</h2>', makeApplyHintFunction(1, 0)],
+  ["<h3", "<h3>", '<h3>\n\n</h3>', makeApplyHintFunction(1, 0)],
+  ["<h4", "<h4>", '<h4>\n\n</h4>', makeApplyHintFunction(1, 0)],
+  ["<h5", "<h5>", '<h5>\n\n</h5>', makeApplyHintFunction(1, 0)],
+  ["<ul", "<ul>", '<ul>\n\n</ul>', makeApplyHintFunction(1, 0)],
+  ["<li", "<li>", '<li>\n\n</li>', makeApplyHintFunction(1, 0)],
+  ["<form", "<form>", '<form>\n\n</form>', makeApplyHintFunction(1, 0)],
+  ["<label", "<label>", '<label></label>', makeApplyHintFunction(0, 7)],
+  ["<button", "<button>", '<button></button>', makeApplyHintFunction(0, 8)],
+  ["<php", "<?php ?>", "<?php  ?>"],
+  ["<phpif", "<?php if {} ?>", "<?php if () { ?>\n\n<?php } ?>", makeApplyHintFunction(0, 10)],
+  ["<phpforeach", "<?php foreach {} ?>", "<?php foreach () { ?>\n\n<?php } ?>", makeApplyHintFunction(0, 15)],
+  ["<phpp", "<?php p() ?>", "<?php p() ?>", makeApplyHintFunction(0, 8)],
+  ["compact", "compact()", "compact()", makeApplyHintFunction(0, 8)],
   "return",
   "namespace",
   "implements",
@@ -39,10 +52,10 @@ const keywords = [
   "private",
   "protected",
   "static",
-  "__construct",
-  'id=""',
-  'class=""',
-  'style=""',
+  ["__construct", "__construct()", "__construct()", makeApplyHintFunction(0, 12)],
+  ['id', 'id=""', 'id=""', makeApplyHintFunction(0, 4)],
+  ['class', 'class=""', 'class=""', makeApplyHintFunction(0, 7)],
+  ['style', 'style=""', 'style=""', makeApplyHintFunction(0, 7)],
   'block',
   'inline-block',
   'justify-content',
@@ -62,6 +75,7 @@ function completeWithKeywords(search_word) {
       return {
         displayText: keyword[1],
         text: keyword[2],
+        hint: keyword[3],
       }
     }
     return keyword
@@ -74,7 +88,7 @@ module.exports = function(editor, options) {
   let current_line = editor.getLine(cursor.line)
   let search_word = current_line
   search_word = search_word.substr(0, cursor.ch)
-  search_word = search_word.replace(/.*?([-_A-Za-z0-9]*)$/, "$1")
+  search_word = search_word.replace(/.*?([-_<A-Za-z0-9]*)$/, "$1")
   if (search_word == "") {
     return {
       list: [],
@@ -84,7 +98,7 @@ module.exports = function(editor, options) {
   }
   let founds = []
   function searchInLine(line_handle) {
-    let words = line_handle.text.split(/[^-_A-Za-z0-9]+/)
+    let words = line_handle.text.split(/[^-<_A-Za-z0-9]+/)
     words = words.filter(function(word) {
       if (word.length <= search_word.length) {
         return false
@@ -102,7 +116,7 @@ module.exports = function(editor, options) {
   founds = completeWithKeywords(search_word).concat(founds)
   founds = [...new Set(founds)]
   let current_word = current_line.substr(cursor.ch)
-  current_word = current_word.replace(/([-_A-Za-z0-9]*)(.*)/, "$1")
+  current_word = current_word.replace(/([-_<A-Za-z0-9]*)(.*)/, "$1")
   current_word = search_word + current_word
   founds = founds.filter(function(found_word) {
     return found_word != current_word
